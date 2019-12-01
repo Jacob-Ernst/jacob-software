1ac2e8d7af2fb10e1efff6034a959af3f48dac79
Jacob Ernst <akumadude.je@gmail.com>
2019-11-30 18:41:53 -0600

    WIP updating gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Jacob Software`,
+    description: `Software by Jacob`,
     author: `@gatsbyjs`
   },
   plugins: [
@@ -41,7 +41,7 @@ module.exports = {
         postCssPlugins: [
           require(`postcss-preset-env`)({ stage: 0 }),
           require('tailwindcss'),
-          ...[purgecss]
+          ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
         ]
       }
     }
